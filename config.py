287ca2facb7a3926706fff6823cd699ecbdd4451
Yosry Ahmed <yosrym93@gmail.com>
2020-04-04 23:00:53 +0200

    Added requirements.txt for python dependencies and removed unused parameters in the config file

--- config.py
+++ config.py
@@ -7,7 +7,4 @@ update_operations_count = 100000
 # System parameters
 physical_blocks_count = 1024
 pages_per_block = 64
-max_update_frequency = 16
-min_update_frequency = 1
-gc_scattering_threshold = 0.001
-gc_erase_count_weight = 100
+
